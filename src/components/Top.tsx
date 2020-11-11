c758715eb5de39fadb1773df3c0d33404fd6972f
wasuken <wasuken1@gmail.com>
2020-11-11 23:14:38 +0900

    impl files sort.

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -28,12 +28,22 @@ const initialState = {
 
 type ActionType = {
 	type: string,
+	orderKey: number,
 	query: string,
 	error: string,
 	payload: Array<FileType>,
 }
 
+let initialAction = {
+	type: "",
+	orderKey: 1,
+	query: "",
+	error: "",
+	payload: [],
+}
+
 const reducer: ((state: InitialStateType, action: ActionType) => InitialStateType) = (state, action) => {
+	let lst: Array<FileType> = [];
 	switch(action.type){
 		case "REQUEST":
 			return {
@@ -67,6 +77,30 @@ const reducer: ((state: InitialStateType, action: ActionType) => InitialStateTyp
 				searchFiles: state.files.filter(file => dt.isBefore(dayjs(file.updated_at))),
 				query: action.query,
 			}
+		case "FILES_ORDER_BY_DATE":
+			lst = JSON.parse(JSON.stringify(state.files));
+			return {
+				...state,
+				loading: false,
+				searchFiles: lst.sort((a, b) => {
+					const a_d = dayjs(a.updated_at);
+					const b_d = dayjs(b.updated_at);
+					if(a_d.isBefore(b_d)){
+						return action.orderKey;
+					}else if(a_d.isAfter(b_d)){
+						return -action.orderKey;
+					}else{
+						return 0;
+					}
+				})
+			};
+		case "FILES_ORDER_BY_NAME":
+			lst = JSON.parse(JSON.stringify(state.files));
+			return {
+				...state,
+				loading: false,
+				searchFiles: lst.sort((a, b) => -(action.orderKey * a.path.localeCompare(b.path)))
+			};
 		default:
 			return state;
 	}
@@ -81,6 +115,7 @@ function Top(){
 			.then(jsonResponse => {
 				if(Array.isArray(jsonResponse)){
 					const action: ActionType = {
+						...initialAction,
 						type: "SUCCESS",
 						query: "",
 						error: "",
@@ -89,6 +124,7 @@ function Top(){
 					dispatch(action);
 				}else{
 					const action: ActionType = {
+						...initialAction,
 						type: "FAILURE",
 						query: "",
 						error: "JSON Response Not Array.",
@@ -103,6 +139,7 @@ function Top(){
 
 	const searchQuery: ((query:string) => void) = ( query: string ) => {
 		const action: ActionType = {
+			...initialAction,
 			type: "SEARCH_QUERY",
 			error: "",
 			query: query,
@@ -113,6 +150,7 @@ function Top(){
 
 	const searchDate: ((query:string) => void) = ( query: string ) => {
 		const action: ActionType = {
+			...initialAction,
 			type: "SEARCH_DATE",
 			error: "",
 			query: query,
@@ -121,11 +159,62 @@ function Top(){
 		dispatch(action);
 	}
 
+	const callFilesOrderByDateFunction: ((e: React.MouseEvent<HTMLInputElement>) => void) = (e) => {
+		e.preventDefault();
+		const action: ActionType = {
+			...initialAction,
+			type: "FILES_ORDER_BY_DATE",
+			error: "",
+			query: "",
+			payload: [],
+		}
+		dispatch(action);
+	}
+
+	const callFilesOrderByNameFunction: ((e: React.MouseEvent<HTMLInputElement>) => void) = (e) => {
+		e.preventDefault();
+		const action: ActionType = {
+			...initialAction,
+			type: "FILES_ORDER_BY_NAME",
+			error: "",
+			query: "",
+			payload: [],
+		}
+		dispatch(action);
+	}
+
+	const radioChangeValue: ((e:React.ChangeEvent<HTMLInputElement>) => void) = (e) => {
+		if(e.target.value === "asc"){
+			initialAction.orderKey = -1;
+		}else{
+			initialAction.orderKey = 1;
+		}
+	}
+
+
+
 	return (
 		<div>
 			<div>
 			<Search searchQuery={searchQuery} searchDate={searchDate} title="title検索" />
 			</div>
+			<div>
+			<input type="submit" value="FilesOrderByDate" onClick={callFilesOrderByDateFunction}/>
+			<input type="submit" value="FilesOrderByName" onClick={callFilesOrderByNameFunction}/>
+
+			<div onChange={radioChangeValue}>
+			<p>
+			<input type="radio" id="asc" name="orderKey" value="asc" defaultChecked={initialAction.orderKey !== 1} />
+			<label htmlFor="asc">asc</label>
+			</p>
+
+			<p>
+			<input type="radio" id="desc" name="orderKey" value="desc" defaultChecked={initialAction.orderKey === 1}/>
+			<label htmlFor="desc">desc</label>
+			</p>
+			</div>
+			</div>
+
 			<div>
 			query: { state.query }
 			</div>
